a23c34403d9dd01e9ad4d5fe28034e9e81517b90
unknown <Curpigeon@358rm18.students.aac.academyart.edu>
2015-08-04 22:20:07 -0700

    Created class for main type odf objects

--- drive.py
+++ drive.py
@@ -14,24 +14,23 @@ from apiclient.http import MediaFileUpload
 from pprint import pprint
 from progressbar import ProgressBar
 import simplejson
-
-SCOPES = 'https://www.googleapis.com/auth/drive'
-CLIENT_SECRET_FILE = 'client_secrets.json'
-TOKEN_FILE = 'token.json'
-
-# TODO{kirill}: Figure out what's going on here?
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+from pathlib import Path
 
 
 class DriveService(object):
+    def __init__(self):
 
-    def __new__(cls):
-        inst = object.__new__(cls)
-        return inst
+        self.SCOPES = 'https://www.googleapis.com/auth/drive'
+        self.CLIENT_SECRET_FILE = 'client_secrets.json'
+        self.TOKEN_FILE = 'token.json'
+
+        try:
+            import argparse
+            self.flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+        except ImportError:
+            self.flags = None
+
+        self.service = self.authenticate()
 
     def _get_credentials(self):
         '''
@@ -50,7 +49,7 @@ class DriveService(object):
             os.makedirs(credential_dir)
 
         # Path to credentil file that store an user token
-        credential_path = os.path.join(credential_dir, TOKEN_FILE)
+        credential_path = os.path.join(credential_dir, self.TOKEN_FILE)
 
         # Save token
         store = oauth2client.file.Storage(credential_path)
@@ -58,16 +57,16 @@ class DriveService(object):
 
         # If faled to retrive token from file create a new one
         if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(os.path.join(credential_dir, CLIENT_SECRET_FILE), SCOPES)
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
+            flow = client.flow_from_clientsecrets(os.path.join(credential_dir, self.CLIENT_SECRET_FILE), self.SCOPES)
+            if self.flags:
+                credentials = tools.run_flow(flow, store, self.flags)
             else: # Needed only for compatability with Python 2.6
                 credentials = tools.run(flow, store)
             print 'Storing credentials to ' + credential_path
 
         return credentials
 
-    def _authenticate(self):
+    def authenticate(self):
         '''
         Returns:
             Authenticated instance of Google Drive API
@@ -78,241 +77,227 @@ class DriveService(object):
 
         return service
 
+class DriveUtils(object):
+    def __init__(self):
+        self.service = DriveService().authenticate()
 
+    def get_file(self, name, parent_id=None):
+        '''
+        Find latest file that match the name. Not looking in trashed
+        '''
 
-def get_file(service, name, parent_id=None):
-    '''
-    Find latest file that match the name. Not looking in trashed
-    '''
-
-    if parent_id:
-        files = service.files().list(q="title = '%s' and trashed = false and '%s' in parents" % (name, parent_id)).execute()['items']
-    else:
-        files = service.files().list(q="title = '%s' and trashed = false" % name).execute()['items']
-
-    if files:
-        if len(files) == 1:
-            print files
-            return files[0]
+        if parent_id:
+            files = self.service.files().list(q="title = '%s' and trashed = false and '%s' in parents" % (name, parent_id)).execute()['items']
         else:
-            print '%d files have "%s" name' % (len(files), name)
+            files = self.service.files().list(q="title = '%s' and trashed = false" % name).execute()['items']
+
+        if files:
+            if len(files) == 1:
+                return files[0]
+            else:
+                print '%d files have "%s" name' % (len(files), name)
+                return
+        else:
+            print 'Nothing found matching "%s"' % name
             return
-    else:
-        print 'Nothing found matching "%s"' % name
-        return
 
-def get_path(service, file):
+class DriveFile(DriveService):
+    def __init__(self, id):
+        super(self.__class__, self).__init__()
+        self.id = id
+        self.file = self._file()
 
-    '''
-    Construct a path to a drive file recursively asking for file parent
-    '''
-    # def parent_title(service, file, path_items=[])
-    parent = file['parents'][0]
-    parent_file = service.files().get(fileId=parent['id']).execute()
-    title = parent_file['title']
+    def _file(self):
+        return self.service.files().get(fileId=self.id).execute()
 
-    path_items = []
-    while title != 'My Drive':
-        path_items.append(title)
-        parent = parent_file['parents'][0]
+    def _list_files(self, drive_file):
+        files = self.service.files().list(
+                q="'%s' in parents and trashed = false" % drive_file['id']
+                ).execute()['items']
+        return files
+
+    def _download_file(self, drive_file, local_file):
+        local_path = local_file.path / Path(drive_file['title'])
+        request = self.service.files().get_media(fileId=drive_file['id'])
+        fh = io.FileIO(str(local_path), mode='wb')
+        media_request = apiclient.http.MediaIoBaseDownload(fh, request)
+
+        print 'Downloading %s' % local_path
+
+        pbar = ProgressBar(maxval=100).start()
+        while True:
+            try:
+              download_progress, done = media_request.next_chunk()
+            except errors.HttpError, error:
+              print 'An error occurred: %s' % error
+              return
+            if download_progress:
+                pbar.update(int(download_progress.progress() * 100))
+            if done:
+              print 'Download Complete'
+              return
+
+    def _download_folder(self, drive_file, local_file):
+
+        files = self._list_files(drive_file)
+
+        # TODO: Need to create folder for downloading files
+
+        for f in files:
+            print f['title']
+            self._download_file(f, local_file)
+
+        print 'All files are downloaded'
+        return files
+
+    def download(self, local_file):
+
+        folder_type = 'application/vnd.google-apps.folder'
+
+        if self.file['mimeType'] == folder_type:
+            self._download_folder(self.file, local_file)
+        else:
+            self.download_file(self.file, local_file)
+
+    def get_path(self):
+
+        '''
+        Construct a path to a drive file recursively asking for file parent
+        '''
+        # def parent_title(service, file, path_items=[])
+        parent = file['parents'][0]
         parent_file = service.files().get(fileId=parent['id']).execute()
         title = parent_file['title']
 
-    path_items.reverse()
-    # import pdb; pdb.set_trace()
-    path = '/My Drive'
-    for i in path_items:
-        path += '/' + i
-    return path
+        path_items = []
+        while title != 'My Drive':
+            path_items.append(title)
+            parent = parent_file['parents'][0]
+            parent_file = service.files().get(fileId=parent['id']).execute()
+            title = parent_file['title']
 
-def upload_file(service, path, parent_id=None):
-  '''
-  Insert new file.
+        path_items.reverse()
+        # import pdb; pdb.set_trace()
+        path = '/My Drive'
+        for i in path_items:
+            path += '/' + i
+        return path
 
-  Args:
-    service: Drive API service instance.
-    title: Title of the file to insert, including the extension.
-    description: Description of the file to insert.
-    parent_id: Parent folder's ID.
-    mime_type: MIME type of the file to insert.
-    path: Path of the file to insert.
-  Returns:
-    Inserted file metadata if successful, None otherwise.
-  '''
-  root, name = os.path.split(path)
+    def update_file(service, file_id, path, new_revision):
+      '''
+      Update an existing file's metadata and content.
 
-  media = MediaFileUpload(path, '', resumable=True)
+      Args:
+        service: Drive API service instance.
+        file_id: ID of the file to update.
+        path: Filename of the new content to upload.
+        new_revision: Whether or not to create a new revision for this file.
+      Returns:
+        Updated file metadata if successful, None otherwise.
+      '''
 
-  body = {
-    'title': name
-  }
-  # Set the parent folder.
-  if parent_id:
-    body['parents'] = [{'id': parent_id}]
-  try:
-    request = service.files().insert(body=body, media_body=media)
-    response = None
-    # Print path to currently copied file
-    print path
-    # Create a progress bar object
-    pbar = ProgressBar(maxval=100).start()
-    while response is None:
-      status, response = request.next_chunk()
-      if status:
-        # Update progress bar
-        pbar.update(int(status.progress() * 100))
-    print "Upload Complete!"
-    return request.execute()
-  except errors.HttpError, error:
-    print 'An error occured: %s' % error
-    return None
+      root, name = os.path.split(path)
 
-def upload_sequence(service, folder, parent_id):
-    '''
-    Upload files one by one in specifed folder
-    '''
-    folder_root, folder_name = os.path.split(folder)
-    files = os.listdir(folder)
+      try:
+        # First retrieve the file from the API.
+        file = service.files().get(fileId=file_id).execute()
 
-    # Create parent sequence directory
-    body = {'title': folder_name,
-            'mimeType': 'application/vnd.google-apps.folder',
-            'parents': [{'id': parent_id}]}
-    drive_sq_root = d.files().insert(body=body).execute()
+        # File's new metadata.
+        file['title'] = name
 
-    for f in files:
-        root, name = folder, f
+        # File's new content.
+        media_body = MediaFileUpload(
+            path, mimetype='', resumable=True)
 
-        upload_file(service=service,
-                    path=os.path.join(root, name),
-                    parent_id=drive_sq_root['id'])
+        # Send the request to the API.
+        updated_file = service.files().update(
+            fileId=file_id,
+            body=file,
+            newRevision=new_revision,
+            media_body=media_body).execute()
+        return updated_file
+      except errors.HttpError, error:
+        print 'An error occurred: %s' % error
+        return None
 
-def download_file(service, file, destination):
-    drive_root = get_path(service, file)
-    drive_path = os.path.join(drive_root, file['title'])
-    local_path = os.path.join(destination, file['title'])
+    def insert_property(service, file_id, key, value, visibility):
+      """
+      Insert new custom file property.
 
-    request = service.files().get_media(fileId=file['id'])
-    fh = io.FileIO(local_path, mode='wb')
-    media_request = apiclient.http.MediaIoBaseDownload(fh, request)
+      """
+      body = {
+        'key': key,
+        'value': value,
+        'visibility': visibility
+      }
 
-    print 'Downloading %s to %s' % (drive_path, local_path)
+      try:
+        p = service.properties().insert(
+            fileId=file_id, body=body).execute()
+        return p
+      except errors.HttpError, error:
+        print 'An error occurred: %s' % error
+      return None
 
-    pbar = ProgressBar(maxval=100).start()
-    while True:
+class LocalFile(DriveService):
+    def __init__(self, path):
+        super(self.__class__, self).__init__()
+        self.path = Path(path)
+
+    def _upload_file(self, path, parent_id):
+
+        media = MediaFileUpload(str(path), '', resumable=True)
+        body = {
+          'title': path.name
+        }
+        # Set the parent folder.
+        if parent_id:
+          body['parents'] = [{'id': parent_id}]
         try:
-          download_progress, done = media_request.next_chunk()
+          request = self.service.files().insert(body=body, media_body=media)
+          response = None
+          # Create a progress bar object
+          pbar = ProgressBar(maxval=100).start()
+          while response is None:
+            status, response = request.next_chunk()
+            if status:
+              # Update progress bar
+              pbar.update(int(status.progress() * 100))
+          print "Upload Complete!"
+          return request.execute()
         except errors.HttpError, error:
-          print 'An error occurred: %s' % error
-          return
-        if download_progress:
-            pbar.update(int(download_progress.progress() * 100))
-        if done:
-          print 'Download Complete'
-          return
+          print 'An error occured: %s' % error
+          return None
 
-def list_files(service, folder):
-    files = d.files().list(
-            q="'%s' in parents and trashed = false" % folder['id']
-            ).execute()['items']
-    return files
+    def _upload_folder(self, parent_id):
+        '''
+        Upload files one by one in specifed folder
+        '''
+        # Create parent sequence directory
+        body = {'title': self.path.name,
+                'mimeType': 'application/vnd.google-apps.folder',
+                'parents': [{'id': parent_id}]}
+        parent = self.service.files().insert(body=body).execute()
 
-def download_sequence(service, folder, destination):
+        for f in self.path.iterdir():
+            self._upload_file(f, parent['id'])
 
-    files = list_files(service, folder)
-
-    for f in files:
-        download_file(service, f, destination)
-
-    print 'All files are downloaded'
-    return files
-
-def update_file(service, file_id, path, new_revision):
-  '''
-  Update an existing file's metadata and content.
-
-  Args:
-    service: Drive API service instance.
-    file_id: ID of the file to update.
-    path: Filename of the new content to upload.
-    new_revision: Whether or not to create a new revision for this file.
-  Returns:
-    Updated file metadata if successful, None otherwise.
-  '''
-
-  root, name = os.path.split(path)
-
-  try:
-    # First retrieve the file from the API.
-    file = service.files().get(fileId=file_id).execute()
-
-    # File's new metadata.
-    file['title'] = name
-
-    # File's new content.
-    media_body = MediaFileUpload(
-        path, mimetype='', resumable=True)
-
-    # Send the request to the API.
-    updated_file = service.files().update(
-        fileId=file_id,
-        body=file,
-        newRevision=new_revision,
-        media_body=media_body).execute()
-    return updated_file
-  except errors.HttpError, error:
-    print 'An error occurred: %s' % error
-    return None
-
-def insert_property(service, file_id, key, value, visibility):
-  """
-  Insert new custom file property.
-
-  """
-  body = {
-    'key': key,
-    'value': value,
-    'visibility': visibility
-  }
-
-  try:
-    p = service.properties().insert(
-        fileId=file_id, body=body).execute()
-    return p
-  except errors.HttpError, error:
-    print 'An error occurred: %s' % error
-  return None
-
-def upload_nuke(service, nuke_scene):
-
-    nuke_scene_dir_id = '0By7scHVmOMWFfl9kTkdRbnFlTTd0UGpIeHNERmozaDVOZXpHckdUazZuQldCZEVKSzQ5SGc'
-    seq_dir_id = '0By7scHVmOMWFfkV4bkQ4emdSMDhwQ0RZR01acXI1Y3Q2X2V0UHl0c0FEMjZTaC1kenlEaWM'
-
-    nuke_root, nuke_name = os.path.split(nuke_scene)
-
-    # Read nuke dependency file
-    with open(os.path.join(nuke_root, '.dependencies'), 'rb') as f:
-        dependencies = simplejson.load(f)
-
-    # Upload thouse dependencies
-    for path in dependencies:
-        root, name = os.path.split(path)
-        upload_sequence(service=service,
-                        folder=root,
-                        parent_id=seq_dir_id)
-
-
-    # Upload the nuke file to drive
-    upload_file(service=service,
-                path=nuke_scene,
-                title=nuke_name,
-                parent_id=nuke_scene_dir_id)
-
-    # Set nuke file properties to reflect this dependencies
-    # insert_property(service, file_id, key, value, visibility)
-
+    def upload(self, parent_id):
+        # Check if file is a folder
+        if self.path.is_dir():
+            self._upload_folder(parent_id)
+        else:
+            self._upload_file(parent_id)
 
 if __name__ == '__main__':
-    service = DriveService()
-    print dir(service)
-    # print service.files().list(q="title = 'CpTestProject'").execute()['items']
+    drive_util = DriveUtils()
+    cp_test_dir = drive_util.get_file('CpTestProject')
+    seq_drive_file = drive_util.get_file('SQ04_SH14_06_RIS.0001.exr')
+
+
+    my_drive_file = DriveFile(drive_util.get_file('test_seq')['id'])
+    download_dir = LocalFile('C:/Users/curpigeon/Desktop/kk_drive')
+    my_drive_file.download(download_dir)
+
+    # test_file = LocalFile('C:/Users/curpigeon/Desktop/kk_drive/test_seq')
+    # test_file.upload(cp_test_dir['id'])
--- nuke_util.py
+++ nuke_util.py
@@ -6,6 +6,8 @@ from drive import d
 
 reload(drive)
 
+nuke_drive_location = {}
+
 def nuke_meta():
     # Collect all external paths
     external_paths = []
@@ -35,16 +37,28 @@ def publish():
     # If soo update file revision instead of creating a new one
     drive_file = drive.get_file(service, name=meta_data['name'],
                                          parent_id=parent_id)
+    # If file already exist create his revision
     if drive_file:
         drive.update_file(service, file_id=drive_file['id'],
                                    path=meta_data['path'],
                                    new_revision=True)
+    # Upload nuke file to drive
     else:
         drive.upload_file(service, path=meta_data['path'],
                                    parent_id=parent_id)
+
+
 
 
     # Upload sequences that use by this nuke file
     for path in meta_data['dependencies']:
         root, name = os.path.split(path)
         drive.upload_sequence(service, root, parent_id)
+
+def embed_id(id):
+    '''
+        Save id to nuke root node as a string parameter
+    '''
+    r = nuke.root()
+    k = nuke.String_Knob("drive_id", "Google Drive ID")
+    r.addKnob(k)
