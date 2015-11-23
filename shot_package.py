534f34aa2a7a1e2fbe31b063e3c7027ea8541aa5
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2015-11-23 02:42:56 -0800

    Added versions

--- shot_package.py
+++ shot_package.py
@@ -6,79 +6,76 @@ from pathlib import Path
 from drive import DriveFile
 from drive import LocalFile
 
-PACKAGE_PATH = 'C:/Users/kkirill2/Desktop/BirdKeeper'
-PACKAGE_FILE_PATH = PACKAGE_PATH + '/package.json'
-PROJECT_DIR = Path('C:/Users/kkirill2/Desktop/BirdKeeper')
+PACKAGE_PATH = os.getcwd()
+PACKAGE_FILE_PATH = PACKAGE_PATH + '/project.json'
+PROJECT_DIR = Path('/Users/amy/Desctop/BirdKeeperTest')
 
 
-class ShotPackage (object):
-	def __init__(self):
-		self.package = self._read_package_file()
+class ProjectPackage (object):
+    def __init__(self):
+        self.package = self._read_package_file()
 
-	def _read_package_file(self):
-		"""
-		returns: JSON object
-		"""
-		with open(PACKAGE_FILE_PATH, 'r') as f:
-			data = f.read()
-		return json.loads(data)
+    def _init(self):
+        # Create loacal project folder specified by user
+        # Download package file from the gdrive
 
-	def _create_path(self, path):
-	    sub_path = os.path.dirname(path)
-	    if not os.path.exists(sub_path):
-	        self._create_path(sub_path)
-	    if not os.path.exists(path):
-	        os.mkdir(path)
+        # TODO(Kirill): Create command line interface with the following features:
+        # init, sync, versionup, sync item etc...
 
-	def _sync_local_package(self):
-		with open(PACKAGE_FILE_PATH, 'w') as f:
-			json.dump(self.package, f, indent=4, sort_keys=True)
+    def _read_package_file(self):
+        """
+        returns: JSON object
+        """
+        with open(PACKAGE_FILE_PATH, 'r') as f:
+            data = f.read()
+        return json.loads(data)
 
-	def _download(self, dfile):
-		
-		dpath = dfile.get_path().relative_to('/My Drive/Bird Keeper/PostProd')
-		fname = dfile.name()
-		file_dir = PROJECT_DIR / dpath
-		file_path = file_dir / fname
-		
-		if not file_dir.exists():
-			self._create_path(str(file_dir))
+    def _create_path(self, path):
+        sub_path = os.path.dirname(path)
+        if not os.path.exists(sub_path):
+            self._create_path(sub_path)
+        if not os.path.exists(path):
+            os.mkdir(path)
 
-		dfile.download(LocalFile(file_path))
+    def _sync_local_package(self):
+        with open(PACKAGE_FILE_PATH, 'w') as f:
+            json.dump(self.package, f, indent=4, sort_keys=True)
 
-		# TODO(kirill): This is just simple check for now.
-		# We need to fo a check for outdated file and if so replace it.
-		# if not file_path.exists():
-		# 	dfile.download(LocalFile(file_path))
-		# else:
-		# 	print fname, 'File already exists'
-	
-	def sync(self):
-		
-		for entity in self.package:
-			for f in entity['files']:
-				dfile = DriveFile(f['id'])
-				
-				if f['version'] < dfile.version():
-					self._download(dfile)
-					f['version'] = dfile.version()
-				else:
-					print "File %s is up to date" % dfile.name()
+    def _download(self, f, dfile):
 
-			for d in entity['dependencies']:
+        dpath = dfile.get_path().relative_to('/My Drive/Bird Keeper/PostProd')
+        fname = dfile.name()
+        file_dir = PROJECT_DIR / dpath
+        file_path = file_dir / fname
 
-				#TODO(Kirll): This is ugly. Also file download twice
-				# Need additional check if file exist
-				dfile = DriveFile(d['id'])
-				
-				if d['version'] < dfile.version():
-					self._download(dfile)
-					d['version'] = dfile.version()
-				else:
-					print "File %s is up to date" % dfile.name()
+        if not file_dir.exists():
+            self._create_path(str(file_dir))
 
-		self._sync_local_package()
+        if file_path.exists():
+            if f['version'] < dfile.version():
+                dfile.download(LocalFile(file_path))
+                f['version'] = dfile.version()
+            elif f['version'] > dfile.version():
+                print 'Need to upload %s since locar version is higher then remote' % f['name']
+            elif f['version'] == dfile.version():
+                print "File %s is up to date" % dfile.name()
+        else:
+            dfile.download(LocalFile(file_path))
+
+    def sync(self):
+
+        for entity in self.package:
+            # Itarate trough all files
+            for f in entity['files']:
+                dfile = DriveFile(f['id'])
+                self._download(f, dfile)
+            # Iterate trought all dependencies
+            for d in entity['dependencies']:
+                dfile = DriveFile(d['id'])
+                self._download(d, dfile)
+
+        self._sync_local_package()
 
 if __name__ == '__main__':
-	s = ShotPackage()
-	s.sync()
+    s = ProjectPackage()
+    s.sync()
